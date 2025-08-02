bee47b3193b053d3c562e94b4f792cc75d883140
IgnacioSala20 <ignaciosala2004@gmail.com>
2025-08-02 13:23:32 -0300

    Eliminación de archivos de configuración de base de datos y docker-compose

--- BD-Pedidos/initdb/001-init.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE DATABASE personas connection LIMIT 10;
-
-create user ignacio superuser createdb createrole encrypted password 'ignacio' connection limit 1;
-GRANT ALL PRIVILEGES ON DATABASE personas TO ignacio;
-\connect personas
-GRANT ALL ON SCHEMA public TO ignacio;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ignacio;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO ignacio;
